e71071a8f825c8d1dd6467d62a2e36dd8c9f6340
Roi Garfunkel <roigarf@gmail.com>
2022-09-11 17:09:47 +0300

    refactored http_handler.go to have better function division

--- handler/http_handler.go
+++ handler/http_handler.go
@@ -11,6 +11,48 @@ import (
 	"reco-exercise-url-shortener/url_generator"
 )
 
+func getLongUrl(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL.String())
+	longUrl, err := redirector.GetOriginalUrl(r.URL.String()[1:])
+	fmt.Println("long url is:", longUrl)
+	var status int
+	switch err {
+	case nil:
+		status = http.StatusFound
+	case storage.NotFoundInDB:
+		status = http.StatusNotFound
+	default:
+		status = http.StatusBadGateway
+	}
+	fmt.Println(longUrl)
+	http.Redirect(w, r, longUrl, status)
+}
+
+func storeUrlAndReturnShort(w http.ResponseWriter, r *http.Request) {
+	reqBody, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	longUrl := string(reqBody)
+	id := url_generator.CreateID(longUrl)
+	fmt.Println("adding new url with id", longUrl, id)
+	err = storage.AddUrl(longUrl, id)
+	if err != nil {
+		_, err := w.Write([]byte("ID is in use"))
+		if err != nil {
+			fmt.Println("Failed to send back the shortened url")
+			return
+		}
+	}
+	shortUrl := base62.Encode(id)
+	shortUrlB := []byte(shortUrl)
+	_, err = w.Write(shortUrlB)
+	if err != nil {
+		// For some reason we can't answer to teh request - just return to keep running.
+		return
+	}
+}
+
 func HandleRequest(w http.ResponseWriter, r *http.Request) {
 	//if r.URL.Path != "/" {
 	//	http.Error(w, "404 not found.", http.StatusNotFound)
@@ -19,38 +61,8 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case "GET":
-		fmt.Println(r.URL.String())
-		longUrl, err := redirector.GetOriginalUrl(r.URL.String()[1:])
-		fmt.Println("long url is:", longUrl)
-		var status int
-		switch err {
-		case nil:
-			status = http.StatusFound
-		case storage.NotFoundInDB:
-			status = http.StatusNotFound
-		default:
-			status = http.StatusBadGateway
-		}
-		fmt.Println(longUrl)
-		http.Redirect(w, r, longUrl, status)
+		getLongUrl(w, r)
 	case "POST":
-		reqBody, err := io.ReadAll(r.Body)
-		if err != nil {
-			log.Fatal(err)
-		}
-		longUrl := string(reqBody)
-		id := url_generator.CreateID(longUrl)
-		fmt.Println("adding new url with id", longUrl, id)
-		err = storage.AddUrl(longUrl, id)
-		if err != nil {
-			_, err := w.Write([]byte("ID is in use"))
-			if err != nil {
-				fmt.Println("Failed to send back the shortened url")
-				return
-			}
-		}
-		shortUrl := base62.Encode(id)
-		shortUrlB := []byte(shortUrl)
-		w.Write(shortUrlB)
+		storeUrlAndReturnShort(w, r)
 	}
 }
