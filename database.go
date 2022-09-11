af1f9dee3d37cf5951b59c0e5fc4d9ee32e1d062
Roi Garfunkel <roigarf@gmail.com>
2022-09-11 11:52:23 +0300

    DB API

--- database.go
+++ database.go
@@ -3,3 +3,40 @@ package reco_exercise_url_shortener
 /*
  * Handle R/W actions on the url data base
  */
+type longUrl string
+type shortUrl string
+type urlMapper map[shortUrl]longUrl
+
+var urlTable urlMapper
+
+/*
+ * Init the database
+ */
+func initMapper(url shortUrl) urlMapper {
+	urlTable := make(urlMapper)
+	return urlTable
+}
+
+/*
+ * Get a short url from a long url in the db
+ */
+func getShortFromLongUrl(url longUrl) {
+
+}
+
+/*
+ * Get a long url from a short url in the db
+ */
+func getLongFromShortUrl(url longUrl) {
+
+}
+
+/*
+* add a pair of long and short url
+ */
+func addUrl(short shortUrl, long longUrl) {
+	_, ok := urlTable[short]
+	if !ok {
+		urlTable[short] = long
+	}
+}
